05ed4c4948cf0958186e7ef1c8d40693c820a4a1
jamal7X7 <jamal7x7@gmail.com>
2018-11-11 23:06:40 +0000

    CSSTransition &       TransitionGroup added

--- src/playground/TodoApp02.js
+++ src/playground/TodoApp02.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/style.sass'
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export default (props) => {
   const [todos, setTodos] = useState([])
@@ -37,31 +37,52 @@ const Todo = (props) => (
   <div className='todoos'>
 
     <div className='todo-wrapper'>
-      { props.todos.map((t, i) => (
-        <div key={i} className='todos'>
-          <div className='color-priority' >
-            <div className='color-dot' />
-          </div>
-          <h3 className='todo'> {t} <span onClick={e => props.handleDone(t)} className='done'>DONE</span></h3>
-        </div>
-        ))
-      }
+      <TransitionGroup >
+        { props.todos.map((t, i) => (
+
+          <CSSTransition
+            in
+            appear
+
+            timeout={300}
+            classNames='todos'
+          // unmountOnExit
+          >
+            <div key={i} className='todos'>
+              <div className='color-priority' >
+                <div className='color-dot' />
+              </div>
+              <h3 className='todo'> {t} <span onClick={e => props.handleDone(t)} className='done'>DONE</span></h3>
+            </div>
+          </CSSTransition>
+
+          ))
+        }
+      </TransitionGroup>
     </div>
   </div>
 )
 
 const Form = (props) => (
 
-  <div>
+  <CSSTransition
+    in
+    appear
 
-    <form action='' onSubmit={props.handleSubmit}>
+    timeout={300}
+    classNames='inp'
+  // unmountOnExit
+  >
 
-      <input type='text' className='input' name='inputText' />
+    <div className='inp'>
+      <form action='' onSubmit={props.handleSubmit}>
 
-      <button>ADD</button>
-    </form>
+        <input type='text' className='input' name='inputText' />
 
-  </div>
+        <button>ADD</button>
+      </form>
+    </div>
+  </CSSTransition>
 )
 
 const date = new Date()
